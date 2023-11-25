2e690947e609773528fdbe92f34bfdf9aecc1c94
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2023-11-24 22:21:49 -0300

    Adicionar endpoint de health check.

---
+++ Server/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuditApp.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "radical :)";
+        }
+    }
+}
